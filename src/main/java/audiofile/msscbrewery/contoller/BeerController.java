efd84d73c0b9e6ec8d3f3e9ca278471eef412252
audiofile100 <amplified100@gmail.com>
2021-11-01 19:07:29 -0600

    more endpoints for beer

--- src/main/java/audiofile/msscbrewery/contoller/BeerController.java
+++ src/main/java/audiofile/msscbrewery/contoller/BeerController.java
@@ -26,7 +26,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<?> handlePost(BeerDto beerDto) {
+    public ResponseEntity<?> handlePost(@RequestBody BeerDto beerDto) {
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -35,5 +35,19 @@ public class BeerController {
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})      // PUT is idempotent
+    public ResponseEntity<?> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
     }
 }
---
+++ src/test/java/audiofile/msscbrewery/controller/BeerControllerTest.java
@@ -0,0 +1,62 @@
+package audiofile.msscbrewery.controller;
+
+import audiofile.msscbrewery.contoller.BeerController;
+import audiofile.msscbrewery.model.BeerDto;
+import audiofile.msscbrewery.service.BeerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(BeerController.class)
+public class BeerControllerTest {
+
+    @MockBean
+    BeerService beerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    BeerDto validBeer;
+
+    @BeforeEach
+    public void setUp() {
+        validBeer = BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Beer1")
+                .beerStyle("PALE_ALE")
+                .upc(123456789012L)
+                .build();
+    }
+
+    @Test
+    public void handlePost() throws Exception {
+        BeerDto beerDto = validBeer;
+        beerDto.setId(null);
+        BeerDto savedDto = BeerDto.builder().id(UUID.randomUUID()).beerName("New Beer").build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        given(beerService.saveNewBeer(any())).willReturn(savedDto);
+
+        mockMvc.perform(post("/api/v1/beer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
+    }
+}
